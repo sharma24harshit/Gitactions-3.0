c9739b0500325c3475bf1260c15260cf86aa5861
unknown <sharma78655533@gmail.com>
2023-01-08 20:01:20 +0530

    ga1

---
+++ github-ci/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      
+      <h1>Counter : {count}</h1>
+      <div>
+        <button onClick={()=>setCount(count-1)} >Dec</button>
+        <button onClick={()=>setCount(count+1)} >Inc</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
